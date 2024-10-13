61ccf92007077598d9a008316bac7c93fb700bf9
Zeddyare <w0433704@nscc.ca>
2024-10-12 21:54:15 -0300

    Working as shown in examples, I suspect that is wrong as I had to shoehorn a 1 in there

--- A2-P2-MobileData.py
+++ A2-P2-MobileData.py
@@ -5,19 +5,39 @@
 # Note, it is not a cumulative charge; your program will figure out which single range the usage falls into, 
 # then calculate the cost based on that range’s cost. 
 
-#Student #:     
-#Student Name:  
+#Student #: 0433704
+#Student Name: Zachary Rudolf
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
+    # Total Data Usage 	                        Rate of Charge 
+    # Up to and including 200Mb 	                $20.00 flat rate 
+    # Over 200Mb and up to and including 500Mb 	$0.105 per Mb 
+    # Over 500Mb and up to and including 1Gb 	    $0.110 per Mb 
+    # Over 1Gb 	                                $118.00 flat rate 
 
+    usage=0.0
+    cost=20.0
+    costPerMb=0.0
+    totalCost=0.0
+    #greetings
+    print("Welcome to Erewhon Mobile.")
+    #input for data used
+    usage=int(input("\nEnter data usage (Mb): "))
+    #if statements to find cost
+    if usage > 200 and usage <=500:
+        costPerMb=(usage-200)*0.105
+    elif usage > 500 and usage <= 1000:
+        costPerMb=(usage-200)*0.110 
+    elif usage > 1000:
+        cost=118.00
+    if costPerMb != 0.0:
+        totalCost= cost+costPerMb+1 # potential error in your examples adding an extra dollar anytime its in a per mb range.
+    else: 
+        totalCost= cost
 
-
-
-
-
-
-
+    #print command for output
+    print("\nTotal charge is: ${0:,.2f}".format(totalCost)) 
     # YOUR CODE ENDS HERE
 
 main()
